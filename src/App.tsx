21ad117bfa1d702237cb2f89c12996c3256fd5ae
qweered <grubian2@gmail.com>
2023-07-31 16:47:12 +0300

    feat: add appshell

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,69 @@
+import { useDisclosure } from "@mantine/hooks";
+import {
+	AppShell,
+	Burger,
+	Group,
+	MantineProvider,
+	UnstyledButton,
+} from "@mantine/core";
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import classes from "./App.module.css";
 
 export default function App() {
-  return <MantineProvider>App</MantineProvider>;
+	const [opened, { toggle }] = useDisclosure();
+	return (
+		<MantineProvider>
+			<AppShell
+				header={{ height: 60 }}
+				navbar={{
+					width: 300,
+					breakpoint: "sm",
+					collapsed: { desktop: true, mobile: !opened },
+				}}
+				padding="md"
+			>
+				<AppShell.Header>
+					<Group h="100%" px="md">
+						<Burger
+							opened={opened}
+							onClick={toggle}
+							hiddenFrom="sm"
+							size="sm"
+						/>
+						<Group ml="xl" gap={20} visibleFrom="sm">
+							<UnstyledButton className={classes.control}>
+								Home
+							</UnstyledButton>
+							<UnstyledButton className={classes.control}>
+								Blog
+							</UnstyledButton>
+							<UnstyledButton className={classes.control}>
+								Contacts
+							</UnstyledButton>
+							<UnstyledButton className={classes.control}>
+								Support
+							</UnstyledButton>
+						</Group>
+					</Group>
+				</AppShell.Header>
+
+				<AppShell.Navbar py="md" px={4}>
+					<UnstyledButton className={classes.control}>Home</UnstyledButton>
+					<UnstyledButton className={classes.control}>Blog</UnstyledButton>
+					<UnstyledButton className={classes.control}>
+						Contacts
+					</UnstyledButton>
+					<UnstyledButton className={classes.control}>
+						Support
+					</UnstyledButton>
+				</AppShell.Navbar>
+
+				<AppShell.Main>
+					Navbar is only visible on mobile, links that are rendered in the
+					header on desktop are hidden on mobile in header and rendered in
+					navbar instead.
+				</AppShell.Main>
+			</AppShell>
+		</MantineProvider>
+	);
 }
